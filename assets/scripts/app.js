9e265d1c7dc59da20f8ed0a651e57126dbe3bf6a
MechaMahmud71 <50962084+MechaMahmud71@users.noreply.github.com>
2019-12-16 23:51:14 +0600

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,23 @@
 
+const loginBtn = document.querySelector(".auBtnLogin");
+const signupBtn = document.querySelector(".auBtnSignup");
+
+window.addEventListener('load', () => {
+    signupBtn.classList.add('activeBtn');
+});
+
+
+loginBtn.addEventListener("click", function () {
+    document.querySelector("#login").style.display = "block";
+    document.querySelector("#signup").style.display = "none";
+    this.classList.add('activeBtn');
+    signupBtn.classList.remove('activeBtn');
+    
+});
+signupBtn.addEventListener("click", function () {
+    document.querySelector("#login").style.display = "none";
+    document.querySelector("#signup").style.display = "block";
+    loginBtn.classList.remove('activeBtn');
+    this.classList.add('activeBtn');
+   
+});
